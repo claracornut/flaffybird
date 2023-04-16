8aaf770a3f7f3da5cd8b97f738b19822fc023172
claracornut <130555968+claracornut@users.noreply.github.com>
2023-04-16 11:15:57 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/flaffybird/MainActivity.kt
+++ app/src/main/java/com/example/flaffybird/MainActivity.kt
@@ -1,11 +1,25 @@
-package com.example.flaffybird
+package com.example.flappybird
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
+    lateinit var gameView: GameView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+        gameView = findViewById<GameView>(R.id.vMain)
+    }
+
+    override fun onPause(){
+        super.onPause()
+        gameView.pause()
+    }
+
+    override fun onResume(){
+        super.onResume()
+        gameView.resume()
     }
 }
